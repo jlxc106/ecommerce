91bcbc20d923070fb5138c5e548402ad269e86ec
jlxc106 <jaylim106@gmail.com>
2018-08-13 18:35:27 -0700

    implement Stripe test-mode

---
+++ src/client/components/SignIn.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SignIn extends Component{
+
+    render(){
+        console.log('signin component not responding')
+        return(
+            <div>Sign In Component to be HERE</div>
+        )
+    }
+}
+
+export default SignIn;
